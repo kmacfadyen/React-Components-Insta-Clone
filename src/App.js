d798614913b4e6623c7bbd86d2622763893a5dde
Kevin MacFadyen <kmacfadyen3@gmail.com>
2022-12-22 18:09:02 -0500

    met MVP

--- src/App.js
+++ src/App.js
@@ -5,15 +5,24 @@
 */
 
 // Import the state hook
-import React from 'react';
+import React, { useState } from 'react';
 // Import the Posts (plural!) and SearchBar components, since they are used inside App component
+import Posts from './components/Posts/Posts';
+import SearchBar from './components/SearchBar/SearchBar';
+
 // Import the dummyData
+import dummyData from './dummy-data';
+
 import './App.css';
 
 const App = () => {
   // Create a state called `posts` to hold the array of post objects, **initializing to dummyData**.
   // This state is the source of truth for the data inside the app. You won't be needing dummyData anymore.
   // To make the search bar work (which is stretch) we'd need another state to hold the search term.
+
+  const [posts, setPosts] = useState(dummyData); // Passing in a state variable and a way to update that state variable (posts, setPosts)
+// Never mutate state directly!
+// setPosts(newPosts); will work! Updating posts will re-render the JSX (the returned data at the bottom)
 
   const likePost = postId => {
     /*
@@ -27,11 +36,34 @@ const App = () => {
         - if the `id` of the post matches `postId`, return a new post object with the desired values (use the spread operator).
         - otherwise just return the post object unchanged.
      */
+    const updatedPosts = posts.map(post => {
+      if (post.id === postId) {
+        return {...post, likes: post.likes + 1}
+      } else {
+        return post;
+      }
+    })
+    setPosts(updatedPosts);
   };
+  /* Higher order functions
+  map: loops over an array and changes something of each element. returns a new array
+  filter: loops over an array and adds elements to a new array if they pass a condition
+  reduce: loops over an array and reduce it to a singular value
+  find: loops over an array and based on a condition, will return the first element of that array
+  
+  */
 
   return (
     <div className='App'>
       {/* Add SearchBar and Posts here to render them */}
+      {/* All tags have to be explicitly closed, aka with /> 
+      posts={} : Posts is the key, whatever is in brackets will be the value 
+      const props = {
+        posts: posts,            We are running this function and passing in props object
+        likePost: likePost
+      } */}
+      <SearchBar />
+      <Posts posts={posts} likePost={likePost} />
       {/* Check the implementation of each component, to see what props they require, if any! */}
     </div>
   );
